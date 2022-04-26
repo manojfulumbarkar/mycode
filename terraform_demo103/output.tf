c907b203eef92747d1418ff3401c207e8926b325
manojfulumbarkar <100022849+manojfulumbarkar@users.noreply.github.com>
2022-04-26 16:51:21 +0530

    Add files via upload

---
+++ terraform_demo103/output.tf
@@ -0,0 +1,4 @@
+output "vpc_id" {
+    value = aws_vpc.demo-vpc.id
+
+}
---
+++ terraform_demo103/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "us-east-2"
+  access_key = "AKIATEX4DCQCGVCW35PF"
+  secret_key = "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"
+}
